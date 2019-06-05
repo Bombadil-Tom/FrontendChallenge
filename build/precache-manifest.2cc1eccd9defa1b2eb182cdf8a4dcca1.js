ea3522a951af0d139ab77899eb36eb84f48d1c6c
Bombadil-Tom <32317018+Bombadil-Tom@users.noreply.github.com>
2019-06-05 07:59:21 -0700

    Add files via upload
    added the build folder

---
+++ build/precache-manifest.2cc1eccd9defa1b2eb182cdf8a4dcca1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c1e0509a10bcb7e43681be94eb039f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c68df50b53c7cbc1280",
+    "url": "/static/css/main.4fcfc3fb.chunk.css"
+  },
+  {
+    "revision": "5c9ff16ec5bee154aee2",
+    "url": "/static/js/2.5286435d.chunk.js"
+  },
+  {
+    "revision": "9c68df50b53c7cbc1280",
+    "url": "/static/js/main.35b17ae4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
